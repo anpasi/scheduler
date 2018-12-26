68681eb33ca56a49db2f42b9515afcebea6fbe61
Antonio <Antonio@192.168.0.162>
2018-12-26 22:56:29 +0000

    First commit

---
+++ scheduler/src/main/java/com/task/system/scheduler/App.java
@@ -0,0 +1,14 @@
+package com.task.system.scheduler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
