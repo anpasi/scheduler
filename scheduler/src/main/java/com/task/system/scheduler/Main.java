3009e727915aa9343aca45630a127b8068578e83
Antonio <Antonio@192.168.0.162>
2018-12-26 23:28:10 +0000

    Basic objects

---
+++ scheduler/src/main/java/com/task/system/scheduler/Main.java
@@ -0,0 +1,24 @@
+package com.task.system.scheduler;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+//
+//		ApplicationContext appContext = 
+//				new ClassPathXmlApplicationContext("spring/config/BeanLocations.xml");
+//		
+//		CSVReader csvReader = (CSVReader)appContext.getBean("csvReader");
+//
+//		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+//		List<City> cities = csvReader.getAllCities(classloader.getResourceAsStream("test_three.csv"));
+		
+        
+    }
+}
