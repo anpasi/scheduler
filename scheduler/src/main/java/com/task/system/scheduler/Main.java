8182e4d0d2d93decf6cb958b2b2e3f57336896c7
Antonio <Antonio@192.168.0.162>
2018-12-27 22:19:42 +0000

    Updated service

--- scheduler/src/main/java/com/task/system/scheduler/Main.java
+++ scheduler/src/main/java/com/task/system/scheduler/Main.java
@@ -1,8 +1,12 @@
 package com.task.system.scheduler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.task.dtos.Task;
 import com.task.services.TaskScheduler;
 
 /**
@@ -17,18 +21,81 @@ public class Main
 		ApplicationContext appContext = 
 				new ClassPathXmlApplicationContext("BeanLocations.xml");
 
-		System.out.println( "taskScheduler.........." );
-
 		TaskScheduler taskScheduler = (TaskScheduler)appContext.getBean("taskScheduler");
 
 		try {
-			taskScheduler.processTasks(null);
+			System.out.println("result = " + taskScheduler.processTasks(null));
+			System.out.println("result = " + taskScheduler.processTasks(new ArrayList<Task>()));
+			System.out.println("result = " + taskScheduler.processTasks(generateTaskList0()));
+			System.out.println("result = " + taskScheduler.processTasks(generateTaskList()));
+			System.out.println("result = " + taskScheduler.processTasks(generateTaskList2()));
+			System.out.println("result = " + taskScheduler.processTasks(generateTaskListCircular()));
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public static List<Task> generateTaskList0(){
 
+		List<Task> list = new ArrayList<Task>();
 
+		Task task = new Task("A", "B");
+		list.add(task);
+		return list;
+	}
 
+	public static List<Task> generateTaskList(){
+
+		List<Task> list = new ArrayList<Task>();
+
+		Task task = new Task("A", "B");
+		list.add(task);
+
+		task = new Task("B", null);
+		list.add(task);
+		
+		task = new Task("C", "D");
+		list.add(task);
+
+		task = new Task("D", null);
+		list.add(task);
+		
+		return list;
+	}
+	
+	public static List<Task> generateTaskList2(){
+
+		List<Task> list = new ArrayList<Task>();
+
+		Task task = new Task("A", "B");
+		list.add(task);
+
+		task = new Task("B", "C");
+		list.add(task);
+		
+		task = new Task("C", null);
+		list.add(task);
+		
+		return list;
+	}
+	
+	public static List<Task> generateTaskListCircular(){
+
+		List<Task> list = new ArrayList<Task>();
+
+		Task task = new Task("A", "B");
+		list.add(task);
+
+		task = new Task("B", "C");
+		list.add(task);
+		
+		task = new Task("C", null);
+		list.add(task);
+		
+		task = new Task("D", "A");
+		list.add(task);
+
+		
+		return list;
 	}
 }
