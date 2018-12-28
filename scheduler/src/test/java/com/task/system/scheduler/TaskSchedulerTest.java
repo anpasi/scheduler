3fce2cf8dc3e280a288cae774a3f6d07c22b09b7
Antonio <Antonio@192.168.0.162>
2018-12-28 09:27:46 +0000

    Updated test name

--- scheduler/src/test/java/com/task/system/scheduler/TaskSchedulerTest.java
+++ scheduler/src/test/java/com/task/system/scheduler/TaskSchedulerTest.java
@@ -1,38 +1,140 @@
 package com.task.system.scheduler;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-/**
- * Unit test for simple App.
- */
-public class TaskSchedulerTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public TaskSchedulerTest( String testName )
-    {
-        super( testName );
-    }
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( TaskSchedulerTest.class );
-    }
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+import com.task.dtos.Task;
+import com.task.services.TaskScheduler;
+
+public class TaskSchedulerTest {
+	
+	private TaskScheduler service;
+	
+	@Before
+	public void beforeEachTest() throws Exception {
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("BeanLocations.xml");
+		service = (TaskScheduler)appContext.getBean("taskScheduler");
+	}
+	
+	@Test
+	public void testExceptionWithCircleDependencies() {
+		try {
+			//When
+			service.processTasks(generateCyclicTaskList());
+			fail();
+		} catch (Exception expected) {
+			//Then
+			assertEquals("Error - this is a cyclic dependency", expected.getMessage());
+		}
+	}
+	
+	@Test
+	public void testEmptyResultWithNullTaskList() throws Exception{
+		//When
+		String result = service.processTasks(null);
+		//Then
+		assertEquals("", result);
+	}
+	
+	@Test
+	public void testEmptyResultWithEmptyTaskList() throws Exception{
+		//Given 
+		List<Task> tasks = Collections.emptyList();
+		//When
+		String result = service.processTasks(tasks );
+		//Then
+		assertEquals("", result);
+	}
+	
+	@Test
+	public void testEmptyResultWithTaskListWithNoDependencies() throws Exception{
+		//When
+		String result = service.processTasks(generateTaskListWithNoDependecies() );
+		//Then
+		assertEquals("[A, B, C, D]", result);
+	}
+	
+	@Test
+	public void testEmptyResultWithTaskListWithDependencies() throws Exception{
+		//When
+		String result = service.processTasks(generateTaskListWithDependencies() );
+		//Then
+		assertEquals("[C, B, A]", result);
+	}
+	
+	/**
+	 * Creates a task list that does not contain any dependecy. So the order of the tasks is 
+	 * the same as the list
+	 * @return list of tasks
+	 */
+	private List<Task> generateTaskListWithNoDependecies(){
+		List<Task> list = new ArrayList<Task>();
+
+		Task task = new Task("A", null);
+		list.add(task);
+
+		task = new Task("B", null);
+		list.add(task);
+		
+		task = new Task("C", null);
+		list.add(task); 
+
+		task = new Task("D", null);
+		list.add(task);
+		
+		return list;
+	}
+	
+	/**
+	 * Creates a task list that in which each task has a dependency, and this dependency is the following task, so the order
+	 * of the execution is the reverse of their order in the list.
+	 * @return list of tasks
+	 */
+	private List<Task> generateTaskListWithDependencies(){
+
+		List<Task> list = new ArrayList<Task>();
+
+		Task task = new Task("A", "B");
+		list.add(task);
+
+		task = new Task("B", "C");
+		list.add(task);
+		
+		task = new Task("C", null);
+		list.add(task);
+		
+		return list;
+	}
+
+	/**
+	 * Creates a task list with a cyclic dependency
+	 * @return list of tasks
+	 */
+	private List<Task> generateCyclicTaskList(){
+		List<Task> list = new ArrayList<Task>();
+		
+		Task task = new Task("A", "B");
+		list.add(task);
+		
+		task = new Task("B", "C");
+		list.add(task);
+		
+		task = new Task("C", null);
+		list.add(task);
+		
+		task = new Task("D", "A");
+		list.add(task);
+		
+		return list;
+	}
+
 }
